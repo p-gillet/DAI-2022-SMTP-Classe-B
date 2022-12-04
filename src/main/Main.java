f7a47ff74245d29bc0168efea43fa8bc609d440a
Miguel Jalube <miguel.jalube@gmail.com>
2022-12-04 12:34:25 +0100

    Mail sended

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,27 +9,17 @@ import java.util.Vector;
  */
 public class Main
 {
-    public static final String FILE_SEPARATOR = ";";
-    public static final int SMTP_PORT =         2525;
-    public static final String SMTP_SERVER =    "localhost";
-    public static final String VICTIMS_FILE = "src/main/sourceFiles/victims.txt";
-    public static final String MESSAGES_FILE = "src/main/sourceFiles/messages.txt";
-    public static final int NUMBER_OF_GROUPS = 4;
-
     public static void main( String[] args ) throws IOException {
-        ConfigurationManager config = new ConfigurationManager(
-                SMTP_SERVER,
-                SMTP_PORT,
-                VICTIMS_FILE,
-                MESSAGES_FILE
-        );
-        PrankGenerator prankGenerator = new PrankGenerator(config,FILE_SEPARATOR);
+        ConfigurationManager config = ConfigurationManager.loadFromFile("src/main/config.txt");
+        PrankGenerator prankGenerator = new PrankGenerator(config,
+           config.getSeparator());
 
-        Vector<Group> groupsVictims = Group.loadGroupsFromFile(config.getVictimsFilePath(), NUMBER_OF_GROUPS);
+        Vector<Group> groupsVictims =
+           Group.loadGroupsFromFile(config.getVictimsFilePath(), config.getNumberOfGroups());
 
         SmtpClient smtpClient = new SmtpClient(config.getSmtpServerAddress(), config.getSmtpServerPort());
 
-        for (int i = 0; i < NUMBER_OF_GROUPS; ++i) {
+        for (int i = 0; i < config.getNumberOfGroups(); ++i) {
             smtpClient.sendPrank(prankGenerator.generatePrank(groupsVictims.get(i)));
         }
 
